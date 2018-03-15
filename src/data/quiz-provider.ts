5581af1178b535ab33826a282dcdb009ac91d46a
Jorge Sánchez <xurxosf@gmail.com>
2018-03-15 11:48:59 +0100

    use quizProvider from GamePage

--- src/data/quiz-provider.ts
+++ src/data/quiz-provider.ts
@@ -1,41 +1,38 @@
 import { Injectable } from '@angular/core';
-//import { Question } from '../domain/question'
-//import { Quiz } from '../domain/quiz'
+import { Question } from '../domain/question'
+import { Quiz } from '../domain/quiz'
+import { QuizRepository } from '../domain/quiz-repository';
  
 @Injectable()
-export class QuizProvider {
+export class QuizProvider implements QuizRepository {
  
     constructor() {
  
     }
- 
-    someFunction(){
-        console.log("I do something useful!");
-    }
 
-    // damelo(){
-    //     // var questions = [];
-    //     // var question1 = new Question (
-    //     // 'https://dummyimage.com/800x600/000/fff?text=UNO', 
-    //     // ['one', 'two', 'three']
-    //     // ,'one');
+    get():Quiz{
+        var questions = [];
+        var question1 = new Question (
+        'https://dummyimage.com/800x600/000/fff?text=UNO', 
+        ['one', 'two', 'three']
+        ,'one');
 
-    //     // var question2 = new Question (
-    //     // 'https://dummyimage.com/800x600/000/fff?text=DOS', 
-    //     // ['aaa', 'bbb', 'ccc']
-    //     // ,'bbb');
+        var question2 = new Question (
+        'https://dummyimage.com/800x600/000/fff?text=DOS', 
+        ['aaa', 'bbb', 'ccc']
+        ,'bbb');
 
-    //     // var question3 = new Question (
-    //     // 'https://dummyimage.com/800x600/000/fff?text=TRES', 
-    //     // ['xxxxx', 'zzzzz', 'wwwww']
-    //     // ,'xxxxx');
+        var question3 = new Question (
+        'https://dummyimage.com/800x600/000/fff?text=TRES', 
+        ['xxxxx', 'zzzzz', 'wwwww']
+        ,'xxxxx');
 
-    //     // questions.push(question1);
-    //     // questions.push(question2);
-    //     // questions.push(question3);
+        questions.push(question1);
+        questions.push(question2);
+        questions.push(question3);
 
-    //     // return new Quiz(questions);
+        return new Quiz(questions);
         
-    // }
+    }
  
 }
--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -14,29 +14,7 @@ export class GamePage {
 
   constructor(public navCtrl: NavController, public quizProvider: QuizProvider) {
 
-    //this.quiz = quizProvider.damelo();
-
-    var questions = [];
-        var question1 = new Question (
-        'https://dummyimage.com/800x600/000/fff?text=UNO', 
-        ['one', 'two', 'three']
-        ,'one');
-
-        var question2 = new Question (
-        'https://dummyimage.com/800x600/000/fff?text=DOS', 
-        ['aaa', 'bbb', 'ccc']
-        ,'bbb');
-
-        var question3 = new Question (
-        'https://dummyimage.com/800x600/000/fff?text=TRES', 
-        ['xxxxx', 'zzzzz', 'wwwww']
-        ,'xxxxx');
-
-        questions.push(question1);
-        questions.push(question2);
-        questions.push(question3);
-
-      this.quiz = new Quiz(questions);
+    this.quiz = quizProvider.get();
   }
 
   answerSelected(answer){
